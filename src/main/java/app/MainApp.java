eb1476e481210af89e25b8f6cc6327bcf18e24a9
ChouraneBD <chouranebd@example.com>
2025-12-10 09:58:37 +0100

    can now runn app

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -1,4 +1,36 @@
 package app;
 
-public class MainApp {
+
+import config.Neo4jConfig;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Neo4jConfig.initialize();
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/login.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Guelya Time");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void stop() {
+        Neo4jConfig.close();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
+
