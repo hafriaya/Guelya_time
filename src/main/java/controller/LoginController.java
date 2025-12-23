1a3acbc833e3d65839677839ba4f0a8258496d51
ChouraneBD <chouranebd@example.com>
2025-12-23 12:13:53 +0100

    remove old

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,36 +1,8 @@
 package controller;
 
-import config.SceneManager;
-import service.SessionService;
-import service.UserService;
-import model.User;
-import javafx.fxml.FXML;
-import javafx.scene.control.*;
+
 
 public class LoginController {
 
-    @FXML private TextField emailField;
-    @FXML private PasswordField passwordField;
-    @FXML private Label messageLabel;
-
-    private final UserService userService = new UserService();
-
-    @FXML
-    public void onLogin() {
-        String email = emailField.getText();
-        String password = passwordField.getText();
-
-        if (userService.login(email, password)) {
-            User user = userService.getUserByEmail(email);
-            SessionService.getInstance().setCurrentUser(user);
-            SceneManager.getInstance().switchTo("dashboard");
-        } else {
-            messageLabel.setText("Email ou mot de passe incorrect.");
-        }
-    }
-
-    @FXML
-    public void goRegister() {
-        SceneManager.getInstance().switchTo("register");
-    }
+    
 }
--- src/main/java/service/SessionService.java
+++ src/main/java/service/SessionService.java
@@ -1,19 +1,7 @@
 package service;
 
-import model.User;
+
 
 public class SessionService {
-    private static SessionService instance;
-    private User currentUser;
 
-    private SessionService() {}
-
-    public static SessionService getInstance() {
-        if (instance == null) instance = new SessionService();
-        return instance;
-    }
-
-    public void setCurrentUser(User user) { this.currentUser = user; }
-    public User getCurrentUser() { return currentUser; }
-    public void clearSession() { currentUser = null; }
 }
