d47d2ecac592cf7c84c58f26e8b205bcf259d354
hafriaya <ms.hafriaya@gmail.com>
2025-12-24 14:44:43 +0100

    import_movies_using_api

---
+++ src/main/java/service/TmdbService.java
@@ -0,0 +1,9 @@
+package service;
+
+public class TmdbService {
+    private static final String API_KEY = "22c0aa4a342097dd598f010fd52eb22c";
+    private static final String BASE_URL = "https://api.themoviedb.org/3";
+
+    
+    
+}
