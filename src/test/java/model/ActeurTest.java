a676e5dd7006bf5533fe860de647b609d24e7fe1
ChouraneBD <chouranebd@example.com>
2026-01-07 14:48:21 +0100

    Updated tests for Acteur model and TmdbService changes

--- src/test/java/model/ActeurTest.java
+++ src/test/java/model/ActeurTest.java
@@ -13,9 +13,9 @@ class ActeurTest {
     @Test
     @DisplayName("Acteur constructor should set all properties")
     void testConstructor() {
-        Acteur acteur = new Acteur("12345", "Leonardo DiCaprio", "/profile.jpg", "Dom Cobb");
+        Acteur acteur = new Acteur(12345L, "Leonardo DiCaprio", "/profile.jpg", "Dom Cobb");
 
-        assertEquals("12345", acteur.getId());
+        assertEquals(12345L, acteur.getId());
         assertEquals("Leonardo DiCaprio", acteur.getName());
         assertEquals("/profile.jpg", acteur.getProfilePath());
         assertEquals("Dom Cobb", acteur.getCharacter());
@@ -24,14 +24,14 @@ class ActeurTest {
     @Test
     @DisplayName("Acteur getters and setters should work")
     void testGettersAndSetters() {
-        Acteur acteur = new Acteur("1", "Name", null, null);
+        Acteur acteur = new Acteur(1L, "Name", null, null);
 
-        acteur.setId("99999");
+        acteur.setId(99999L);
         acteur.setName("Tom Hanks");
         acteur.setProfilePath("/tom.jpg");
         acteur.setCharacter("Forrest Gump");
 
-        assertEquals("99999", acteur.getId());
+        assertEquals(99999L, acteur.getId());
         assertEquals("Tom Hanks", acteur.getName());
         assertEquals("/tom.jpg", acteur.getProfilePath());
         assertEquals("Forrest Gump", acteur.getCharacter());
@@ -40,9 +40,9 @@ class ActeurTest {
     @Test
     @DisplayName("Acteur should handle null values")
     void testNullValues() {
-        Acteur acteur = new Acteur(null, null, null, null);
+        Acteur acteur = new Acteur();
 
-        assertNull(acteur.getId());
+        assertEquals(0L, acteur.getId());
         assertNull(acteur.getName());
         assertNull(acteur.getProfilePath());
         assertNull(acteur.getCharacter());
@@ -51,11 +51,25 @@ class ActeurTest {
     @Test
     @DisplayName("Acteur profilePath can be null for actors without photos")
     void testNullProfilePath() {
-        Acteur acteur = new Acteur("123", "Unknown Actor", null, "Extra");
+        Acteur acteur = new Acteur(123L, "Unknown Actor", null, "Extra");
 
-        assertEquals("123", acteur.getId());
+        assertEquals(123L, acteur.getId());
         assertEquals("Unknown Actor", acteur.getName());
         assertNull(acteur.getProfilePath());
         assertEquals("Extra", acteur.getCharacter());
+    }
+    
+    @Test
+    @DisplayName("getFullProfileUrl should return valid URL when profilePath exists")
+    void testGetFullProfileUrl() {
+        Acteur acteur = new Acteur(1L, "Test", "/abc123.jpg", null);
+        assertEquals("https://image.tmdb.org/t/p/w185/abc123.jpg", acteur.getFullProfileUrl());
+    }
+    
+    @Test
+    @DisplayName("getFullProfileUrl should return null when profilePath is null")
+    void testGetFullProfileUrlNull() {
+        Acteur acteur = new Acteur(1L, "Test", null, null);
+        assertNull(acteur.getFullProfileUrl());
     }
 }
