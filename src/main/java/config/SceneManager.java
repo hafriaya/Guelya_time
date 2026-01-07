9cdb05fb6d555e49f54997bca2d87a2e21228325
Chourane_Badreddine <chouranebd@gmail.com>
2026-01-07 12:00:06 +0100

    Connect movie cards to details page

--- src/main/java/config/SceneManager.java
+++ src/main/java/config/SceneManager.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Film;
+import controller.MovieDetailsController;
 
 import java.io.IOException;
 
@@ -23,6 +25,21 @@ public class SceneManager {
     public void switchTo(String fxmlName) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/" + fxmlName + ".fxml"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void showMovieDetails(Film film) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/movie-details.fxml"));
+            Parent root = loader.load();
+            
+            MovieDetailsController controller = loader.getController();
+            controller.setFilm(film);
+            
             primaryStage.setScene(new Scene(root));
             primaryStage.show();
         } catch (IOException e) {
